d47d9cdbd204bf0018dad3cc7d9a0808868ea195
nurhana <nur.hana@metropolia.fi>
2024-08-29 20:01:18 +0300

    mod3 tehtävien  palautus

--- mod03/teht02.py
+++ mod03/teht02.py
@@ -1,6 +1,6 @@
 #Kirjoita ohjelma, joka kysyy käyttäjältä laivan hyttiluokan (LUX, A, B, C) ja
 # tulostaa sen sanallisen kuvauksen alla olevan luettelon mukaisesti.
-# Tehtävässä on käytettävä if/elif/else-toistorakennetta.
+# Tehtävässä on käytettävä if/elif/else-toistorakennetta..
 
 #LUX on parvekkeellinen hytti yläkannella.
 #A on ikkunallinen hytti autokannen yläpuolella.
