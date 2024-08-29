cb51348cace7dc46e82bc7767deb135501243f9b
nurhana <nur.hana@metropolia.fi>
2024-08-29 19:59:21 +0300

    mod2 tehtävien  palautus

---
+++ mod03/teht01.py
@@ -0,0 +1,14 @@
+# 1.Kirjoita ohjelma, joka kysyy kalastajalta kuhan pituuden senttimetreinä.
+# Jos kuha on alamittainen, ohjelma käskee laskea kuhan takaisin järveen ilmoittaen samalla käyttäjälle,
+# montako senttiä alimmasta sallitusta pyyntimitasta puuttuu.
+# Kuha on alamittainen, jos sen pituus on alle 37 cm.
+
+kuha = float(input("Mikä on kuhan pituus cm: "))
+if kuha < 37:
+    print("Kuha on alimittainen, laskee kuhan takaisin järven.")
+    paino = 37 - kuha
+    print(f"Alimmasta sallitusta pyyntimitasta puuttuu {paino} cm ")
+else:
+    print("Kuha on sallittu pyyntimittainen")
+
+
---
+++ mod03/teht03.py
@@ -0,0 +1,24 @@
+#Kirjoita ohjelma, joka kysyy käyttäjän biologisen sukupuolen ja hemoglobiiniarvon (g/l).
+# Ohjelma ilmoittaa, onko hemoglobiiniarvo alhainen, normaali vai korkea.
+
+#Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
+#Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
+
+sukupuoli = input("Mikä on sinun biologisen sukupuoli: ")
+hemoglobiiniarvo = int(input("Mikä on sinun hemoglobiiniarvo (g/l): "))
+
+if sukupuoli == "nainen":
+
+    if hemoglobiiniarvo <117:
+        print("Hemoglobiiniarvo on alhainen")
+    elif hemoglobiiniarvo >= 175:
+        print("Hemoglobiiniarvo on korkea")
+    else:
+        print("Hemoglobiiniarvo on normaali")
+elif sukupuoli == "mies":
+    if hemoglobiiniarvo <= 134:
+        print("Hemoglobiiniarvo on alhainen")
+    elif hemoglobiiniarvo >= 195:
+        print("Hemoglobiiniarvo on korkea")
+    else:
+        print("Hemoglobiiniarvo on normaali")
