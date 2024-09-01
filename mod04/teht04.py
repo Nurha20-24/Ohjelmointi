45f2d222a08bc2bf3cc0023fc97cbf780f07589c
nurhana <nur.hana@metropolia.fi>
2024-09-01 23:41:57 +0300

    mod4 tehtävien palautus

--- mod04/teht04.py
+++ mod04/teht04.py
@@ -5,7 +5,7 @@ import random
 luku = random.randint(1, 10)
 
 while True:
-    arvaus = int(input("Arvaa luku väliltä 1-10: "))
+    arvaus = int(input("Arvaa luku väliltä 1..10: "))
     if arvaus > luku:
         print("Liian suuri arvaus")
     elif arvaus < luku:
